b820bd0261a6c415beb785b1aa7256c925ea062e
Mary-Anne Triggs <maryannetriggs@gmail.com>
2019-11-13 14:33:42 +0000

    initial tag seeds added and models improved

--- blogs/models.py
+++ blogs/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+# class Comment(models.Model):
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.tag}'
+
 class Blog(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     subtitle = models.CharField(max_length=200)
     author = models.CharField(max_length=50)
+    date_published = models.DateField(auto_now=False)
     images = models.CharField(max_length=200)
     story = models.CharField(max_length=50)
-    tags = models.CharField(max_length=50)
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='blogs',
+        blank=True
+    )
+
+    def __str__(self):
+        return f'{self.title}'
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class ListView(View):
+
+    def get(self, request):
+        blogs = Blog.objects.all()
+
+        return render(request, 'index.html', {'blogs': blogs})
