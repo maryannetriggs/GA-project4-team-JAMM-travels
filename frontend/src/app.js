07560835d7f9eb32c34e2edad7f1f7978e55cdac
Mary-Anne Triggs <maryannetriggs@gmail.com>
2019-11-14 15:39:51 +0000

    tag seeds updated

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './style.scss'
+
+import Home from './components/common/Home'
+
+const App = () => (
+  <Home/>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
